b2e03f19efd13e2755518539e58c59ea6ec512da
SwampStats <SwampStats@users.noreply.github.com>
2018-02-06 21:33:07 -0600

    converted to line plot with few points

--- rw_visual.py
+++ rw_visual.py
@@ -6,5 +6,9 @@ from random_walk import RandomWalk
 rw = RandomWalk()
 rw.fill_walk()
 plt.figure(figsize=(20, 12))
-plt.scatter(rw.x_values, rw.y_values, s=1)
+#plt.scatter(rw.x_values, rw.y_values, s=1)
+#now try with plot
+plt.plot(rw.x_values, rw.y_values, linewidth=1)
+
 plt.show()
+
