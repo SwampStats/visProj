bb069a3115c7812d97720b233cfe6cb112077f94
SwampStats <SwampStats@users.noreply.github.com>
2018-02-14 22:56:45 -0600

    refactored using list comprehensions

--- die_visual.py
+++ die_visual.py
@@ -5,22 +5,20 @@ from die import Die
     # Create a D6.
 die = Die()
    #make some rolls, and store resuls in a list.
+
 results = []
-for roll_num in range(1000):
-    result = die.roll()
-    results.append(result)
+results= [die.roll() for roll_num in range(1000)]
 
     # Analyze the results.
-    frequencies = []
-    for value in range(1, die.num_sides+1):
-        frequency = results.count(value)
-        frequencies.append(frequency)
+frequencies = []
+frequencies = [results.count(value) for value in range(1, die.num_sides + 1)]
 
 # Visualize the results.
 hist = pygal.Bar()
 
 hist.title = "Results of rolling one D6 1000 times."
-hist.x_labels = ['1', '2', '3', '4', '5', '6']
+#hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_labels=[x for x in range(1,6)]
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
