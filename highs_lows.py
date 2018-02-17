f41b43b84ab095bfe3befb3a7ef8dee276034bdd
SwampStats <SwampStats@users.noreply.github.com>
2018-02-17 10:48:03 -0600

    added low temp

--- highs_lows.py
+++ highs_lows.py
@@ -1,15 +1,42 @@
 import csv
+from datetime import datetime
+from matplotlib import pyplot as plt
 
-#Get high temperature from file.
-filename = 'sitka_weather_07-2014.csv'
+#Get dates, high, and low temperatures from file.
+filename = 'sitka_weather_2014.csv'
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 #    print(header_row)
 
 
-    highs = [int(row[1]) for row in reader]
-    print(highs)
+    dates, highs, lows = [],[],[]
+    for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
+        high = int(row[1])
+        highs.append(high)
+
+        low = int(row[3])
+        lows.append(low)
+#    print(highs)
     
 #    for index, column_header in enumerate(header_row):
 #        print(index, column_header)
+
+# Plot data.
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
+# Format plot
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
+plt.xlabel('', fontsize=16)
+
+fig.autofmt_xdate()
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
+
+
